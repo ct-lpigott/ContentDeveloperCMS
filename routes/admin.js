181b27087d89943ab56c86ec33aafcab9b9a7747
Laura Pigott <pigottlaura@gmail.com>
2016-12-15 18:30:15 +0000

    Creating layout, header and footer view components
    So that the admin and general views can extend from them

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get("/", function(req, res, next){
     console.log("Request recieved in admin route");
-    res.render("admin", {});
+    res.render("admin", {pageTitle: "Admin Panel"});
 });
 
 module.exports = router;
