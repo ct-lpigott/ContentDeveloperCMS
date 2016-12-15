886bb71b8b37668e854be7da3a36a987545c44a9
Laura Pigott <pigottlaura@gmail.com>
2016-12-15 17:48:22 +0000

    Setting up basic express app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var app = require("express")();
+
+app.get("/", function(req, res, next){
+    console.log("Request Recieved");
+    res.send("<h1>Content Developer CMS</h1>");
+});
+
+app.listen(3000, function () {
+  console.log("Listening on localhost:3000");
+})
+
 console.log("Initialised");
