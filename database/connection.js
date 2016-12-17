9d6eb9ac143ad607bb545835225dfeabb9a1a06d
Laura Pigott <pigottlaura@gmail.com>
2016-12-17 18:20:05 +0000

    Logging out Connection String and IP Address

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,13 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connectionString = process.env.DB_CONNECTION_STRING;
+
+console.log("CONNECTION STRING", connectionString);
+
+
+require("dns").lookup(require("os").hostname(), function (err, address, fam) {
+  console.log("IP ADDRESS:", address);
+})
 
 var connection = mysql.createConnection(connectionString);
 
