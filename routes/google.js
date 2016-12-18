7a83717b2d8c95741b3c07117bb5fb909e2290f3
Laura Pigott <pigottlaura@gmail.com>
2016-12-18 21:56:50 +0000

    Creating new user based on access token

--- routes/google.js
+++ routes/google.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var dbconn = require("../database/connection.js");
+var googleOAuthClient = require("../google/googleOAuthClient");
 
 router.get("/oauthRedirectURL", function(req, res, next){
-    res.send("Google ID: " + req.param.code);
+    var authCode = req.query.code;
+
+    googleOAuthClient.getToken(authCode, function(error, token) {
+        if (error) {
+            console.log("Error while trying to retrieve access token", error);
+        } else {
+            dbconn.query("INSERT into Users(google_token) VALUES(" + token + ")", function(err, rows, fields){
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(rows);
+                }
+            });
+            oauth2Client.credentials = token;
+        }
+    });
+    res.send("Google ID: " + req.query.code);
 });
 
 module.exports = router;
