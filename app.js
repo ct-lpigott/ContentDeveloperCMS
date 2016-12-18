89f042382660d4379c94369b8e853cb6dbb277ed
Laura Pigott <pigottlaura@gmail.com>
2016-12-18 21:30:53 +0000

    Adding Google route to server

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var pug = require("pug");
 
 var general_route = require("./routes/general.js");
 var admin_route = require("./routes/admin.js");
+var google_route = require("./routes/google.js");
 
 app.use("/", general_route);
 app.use("/admin", admin_route);
+app.use("/google", google_route);
 
 app.set("view engine", "pug");
 app.set("views", "./views");
