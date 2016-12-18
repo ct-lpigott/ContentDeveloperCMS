b8a4102e012e694c8af4dd59ce4de409c54648b9
Laura Pigott <pigottlaura@gmail.com>
2016-12-18 22:49:04 +0000

    Inserting new user to DB

--- routes/google.js
+++ routes/google.js
@@ -10,16 +10,19 @@ router.get("/oauthRedirectURL", function(req, res, next){
         if (error) {
             console.log("Error while trying to retrieve access token", error);
         } else {
-            dbconn.query("INSERT into User(google_token) VALUES(" + token + ")", function(err, rows, fields){
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log(rows);
-                }
-                res.send(err, rows, fields);
-            });
+            googleOAuthClient.credentials = token;
         }
     });
+
+    dbconn.query("INSERT into User(google_authcode) VALUES(" + dbconn.escape(authCode) + ")", function(err, rows, fields){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(rows);
+        }
+    });
+
+    res.send("Logged In");
 });
 
 module.exports = router;
