bc04c00e81fd3d48cb342d3091d31323fd669374
Laura Pigott <pigottlaura@gmail.com>
2016-12-19 00:20:05 +0000

    Getting and displaying user info
    i.e. Name, profile image

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var dbconn = require("../database/connection.js");
+var google = require("googleapis");
+var googleOAuth = require("../google/googleOAuthClient");
 
 router.get("/", function(req, res, next){
     console.log("Request recieved in admin route");
-    dbconn.query("SHOW TABLES", function (err, rows, fields){
+    dbconn.query("SELECT * FROM User WHERE id=" + dbconn.escape(req.params.id), function (err, rows, fields){
         if(err){
             console.log(err);
         } else {
-            console.log(rows);
+            if(rows.length > 0){
+                var plus = google.plus('v1');
+                var usersGoogleAuthToken = JSON.parse(rows[0].google_auth_token);
+
+                var oauth2Client = googleOAuth.oauth2Client;
+                oauth2Client.credentials = usersGoogleAuthToken;
+
+                plus.people.get({
+                    userId: 'me',
+                    auth: oauth2Client
+                }, function (err, user) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log(user);
+                        res.render("admin", {
+                            pageTitle: "Admin Panel",
+                            userDisplayName: user.displayName,
+                            userProfileImage: user.image.url.replace("?sz=50", "")
+                        });
+                    }
+                });
+            }
         }
-        res.render("admin", {pageTitle: "Admin Panel", dbTables: rows});
     });
 });
 
--- routes/google.js
+++ routes/google.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var google = require("googleapis");
+
 var dbconn = require("../database/connection.js");
 var googleOAuth = require("../google/googleOAuthClient");
 
@@ -10,18 +12,48 @@ router.get("/oauthRedirectURL", function(req, res, next){
         if (error) {
             console.log("Error while trying to retrieve access token", error);
         } else {
-            var jsonToken = JSON.stringify(token);
-            dbconn.query("INSERT into User(google_auth_token) VALUES(" + dbconn.escape(jsonToken) + ")", function(err, rows, fields){
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log(rows);
-                }
+            var plus = google.plus('v1');
+            
+            var oauth2Client = googleOAuth.oauth2Client;
+            oauth2Client.credentials = token;
+
+            plus.people.get({
+                userId: 'me',
+                auth: oauth2Client
+            }, function (err, user) {
+                dbconn.query("SELECT * FROM User WHERE google_profile_id=" + dbconn.escape(user.id), function(err, rows, fields){
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        var jsonToken = JSON.stringify(token);
+                        if(rows.length > 0){
+                            console.log("This is an existing user");
+
+                            dbconn.query("UPDATE User SET google_auth_token = " + dbconn.escape(jsonToken) + " WHERE id = " + dbconn.escape(rows[0].id), function(err, rows, fields){
+                                if(err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("Existing user auth token updated");
+                                }
+                            });
+                        } else {
+                            console.log("This is a new user");
+
+                            dbconn.query("INSERT into User(google_profile_id, google_auth_token) VALUES(" + dbconn.escape(user.id) + ", " + dbconn.escape(jsonToken) + ")", function(err, rows, fields){
+                                if(err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("New user created in DB");
+                                }
+                            });
+                        }
+                        res.redirect("/admin?id=" + rows[0].id);
+                    }
+                });
             });
+            
         }
     });
-
-    res.send("Logged In");
 });
 
 module.exports = router;
