31e95991e84c64bb8ef9cd3d11a47bccf0250af2
Laura Pigott <pigottlaura@gmail.com>
2017-01-24 01:01:38 +0000

    Recommenting google OAuth and google route

--- google/googleOAuth.js
+++ google/googleOAuth.js
@@ -1,13 +1,35 @@
+// Requiring the file system module, so that this module
+// can have access to the file system (to read in the credentials
+// of the projects client secret keys)
 var fs = require("fs");
-var google = require("googleapis");
+
+// Requiring the Google auth library, which will be used to 
+// generate new OAuth2 clients (to be used to authorise, and
+// make requests of the Google APIs throughout the server)
 var googleAuth = require("google-auth-library");
 
+// Storing the client secret data for the servers Google projects
+// so that they can be used when setting up new OAuth2Clients
 var clientSecretData = JSON.parse(fs.readFileSync("./google/client_secret.json"));
+
+// Setting the redirectURL (which will be used by the OAuth2
+// request to return a user to the server following a successful login)
+// based on that specified in the environment variable i.e. this may
+// vary depending on where the server is running
 var redirectURL = process.env.GOOGLE_OAUTH_REDIRECT_URL;
 
+// Setting the export of this module to be equal to an object, which 
+// contains two methods - one to generate a new OAuth URL, and the other
+// to generate new OAuth2Clients
 module.exports = {
   generateOAuthUrl: function(){
+    // Generating a new OAuth2Client using the other method defined in this object
     var newOAuth2Client = this.generateOAuth2Client();
+
+    // Creating a new OAuthURL (which the user will use to access the login page
+    // on Google to authorise the server to access their account) by using the built
+    // in method of the Google Auth libraries OAuth2Client, passing in the access type
+    // and API scope that this login would authorise
     var oauthURL = newOAuth2Client.generateAuthUrl({
         access_type: "offline",
         scope: [
@@ -16,15 +38,25 @@ module.exports = {
         "https://www.googleapis.com/auth/userinfo.email"
         ]
     });
+
+    // Returning the OAuth URL to the caller
     return oauthURL;
   },
   generateOAuth2Client: function(){
+    // Generating a new Auth object, using the Google Auth library
     var auth = new googleAuth();
+
+    // Creating a new OAuth2Client using the OAuth2 method of the 
+    // auth object, passing in the servers client id, client secret,
+    // and the redirect URL to return the user to the server following
+    // successful login
     var newOAuth2Client = new auth.OAuth2(
       clientSecretData.web.client_id,
       clientSecretData.web.client_secret,
       redirectURL
     );
+
+    // Returning the new OAuth2Client to the caller
     return newOAuth2Client;
   }
 };
--- routes/google.js
+++ routes/google.js
@@ -1,70 +1,122 @@
-var express = require('express');
-var router = express.Router();
+// Creating a new router through the express module. This router
+// will be used within this module so set up the various requests
+// which this route will accept.
+var router = require('express').Router();
+
+// Requiring the googleapis module, so that it can be used within
+// within this module to make requests to specific Google APIs
+// i.e. to make request to the Plus API, to get the users email address
 var google = require("googleapis");
 
+// Requiring the custom database connection module, so that the one
+// connection to the database can be reused throughout the application.
 var dbconn = require("../database/connection.js");
+
+// Requiring the custom google OAuth module, which exports an object with 
+// a method to generate a new oauth url, and a method which returns
+// a new OAuth2Client.
 var googleOAuth = require("../google/googleOAuth");
 
+// Request from the Google API, returning the user from a successful login to
+// their Google account
 router.get("/oauthRedirectURL", function(req, res, next){
+    // Temporarily storing the auth code, included in the request, as this will be used
+    // request the access token from the Google API i.e. it proves that the user has 
+    // authorised us to do so
     var authCode = req.query.code;
 
+    // Creating a new OAuth2Client, using the generateOAuth2Client() method of the custom
+    // googleOAuth module
     var oauth2Client = googleOAuth.generateOAuth2Client();
     
-    oauth2Client.getToken(authCode, function(error, token) {
-        if (error) {
+    // Requesting an access token from the Google API, using the access token returned
+    // from the users login to their Google account
+    oauth2Client.getToken(authCode, function(err, token) {
+        if(err) {
             console.log("Error while trying to retrieve access token", error);
         } else {
+            // Accessing the Google Plus API, through the google apis module
             var plus = google.plus('v1');
             
+            // Setting the credentials of the OAuth2Client to be equal to
+            // the token returned from the request
             oauth2Client.credentials = token;
 
+            // Making a request to the Google Plus API, to get the profile information
+            // relating to this user. Passing the oauth2Client created above as the
+            // authentication for this request.
             plus.people.get({
                 userId: 'me',
                 auth: oauth2Client
             }, function (err, user) {
-                console.log(user.emails[0].value);
+                // Querying the database, to find the user with the email address that matches this user
                 dbconn.query("SELECT * FROM User WHERE email_address=" + dbconn.escape(user.emails[0].value), function(err, rows, fields){
                     if(err) {
-                        console.log(err);
+                        console.log("Unable to find user that has this Google profiles email address. " + err);
                     } else {
+                        // Temporarily storing the users access token in a JSON string
                         var jsonToken = JSON.stringify(token);
                         
+                        // Checking if any results were returned from the database i.e. has the user connected
+                        // with this Google account been fount
                         if(rows.length > 0){
-                            req.userID = rows[0].id;
                             console.log("This is an existing user");
+
+                            // Storing the userID of this user on the request object, so that it can
+                            // be used throughout this route
+                            req.userID = rows[0].id;
+                            
+                            // Updating this users google profile ID and auth token, based on those returned from 
+                            // the Google API request
                             dbconn.query("UPDATE User SET google_profile_id = " + user.id + ", google_auth_token = " + dbconn.escape(jsonToken) + " WHERE id = " + dbconn.escape(rows[0].id), function(err, result){
                                 if(err) {
-                                    console.log(err);
+                                    console.log("Unable to update users Google profile ID and auth token. " + err);
                                 } else {
                                     console.log("Existing user auth token updated");
+
+                                    // Passing this request on to the next stage of this route
                                     next();
                                 }
                             });
                         } else {
                             console.log("This is a new user");
 
+                            // Creating a new user in the database, using the email address, Google profile ID
+                            // and auth access token provided by the request to the Google API
                             dbconn.query("INSERT into User(email_address, google_profile_id, google_auth_token) VALUES(" + dbconn.escape(user.emails[0].value) + ", " + dbconn.escape(user.id) + ", " + dbconn.escape(jsonToken) + ")", function(err, result){
                                 if(err) {
                                     console.log(err);
                                 } else {
                                     console.log("New user created in DB");
+
+                                    // Storing the userID of this user on the request object, so that it can
+                                    // be used throughout this route
                                     req.userID = result.insertId;
+                                    
+                                    // Passing this request on to the next stage of this route
                                     next();
                                 }
                             });
                         }
                     }
                 });
-            });
-            
+            });  
         }
     });
 });
-
+// Continued - Request from the Google API, returning the user from a successful login to
+// their Google account
 router.get("/oauthRedirectURL", function(req, res, next){
+    // Checking that a userID exists on the request object
     if(req.userID != null){
+        // Redirecting this user to the admin panel, using their userID as a parameter
         res.redirect("/admin/" + req.userID);
+    } else {
+        console.log("No user was found");
+        res.send("Unable to login");
     }
 });
 
+// Exporting the router that was set up in this file, so that it can be included
+// in the app.js file and specified as the route for all requests to the "/google" route
 module.exports = router;
