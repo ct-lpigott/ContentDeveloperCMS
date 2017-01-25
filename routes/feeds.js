123e7a19133cd3a4641aadb2184c7d10093886eb
Laura Pigott <pigottlaura@gmail.com>
2017-01-25 21:23:34 +0000

    Removing unnecessary imports
    No longer required, as per changes made in previous commits

--- routes/feeds.js
+++ routes/feeds.js
@@ -3,20 +3,6 @@
 // which this route will accept.
 var router = require('express').Router();
 
-// Requiring the file system module, so that this route can have access
-// to the file system of the server i.e. to be able to create, open, edit 
-// and save project files to the /projects directory
-var fs = require("fs");
-
-// Requiring the custom database connection module, so that the one
-// connection to the database can be reused throughout the application.
-var dbconn = require("../database/connection.js");
-
-// Requiring the custom google OAuth module, which exports an object with 
-// a method to generate a new oauth url, and a method which returns
-// a new OAuth2Client.
-var googleOAuth = require("../google/googleOAuth");
-
 // All
 router.use(require("./components/feeds/request-validation.js"));
 router.use(require("./components/feeds/database-only.js"));
