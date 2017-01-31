44bb7bc9371f25c2daa06d08465f3daea5ab970e
Laura Pigott <pigottlaura@gmail.com>
2017-01-31 18:00:21 +0000

    Infinite encapsulation for delete

--- routes/components/feeds/feeds-delete.js
+++ routes/components/feeds/feeds-delete.js
@@ -8,80 +8,208 @@ var router = require('express').Router();
 // and save project files to the /projects directory
 var fs = require("fs");
 
+var validation = require("./../validation.js");
+
 // Request to delete a project
 router.delete("/:projectID", function(req, res, next){
     res.send("DELETE request received from userID=" + req.userID + " for projectID=" + req.params.project);
 });
 
-// Request to delete a collection from a project
-router.delete("/:projectID/:collection", function(req, res, next){
-    res.send("DELETE request received from userID=" + req.userID + " for projectID=" + req.params.project + " collection=" + req.params.collection);
-});
+// Request to create a new item or collections CONTENT in a project
+router.delete("/:projectID/*", function(req, res, next){
+    // Checking that the request contains more than just the project ID
+    if(req.allParams.length > 1){
+        // Checking that the fileData (i.e. the current content of the project) has
+        // been loaded in the preload-filedata route
+        if(req.fileData.content){                
+            // Calling the deleteItem method (which is defined directly below this)
+            // to delete the requested item. Passing it the current content file data, and the
+            // encapsulationData (i.e. all the parameters of the request, excluding the
+            // projectID parameter - by slicing the array so it starts at index 1), 
+            // The purpose of using a function, that will only ever be available within this 
+            // middleware, is that I need the file data content to be passed by reference, 
+            // as opposed to JavaScripts default "pass by value" approach. By passing 
+            // this content to a function, any updates to its properties will be reflected 
+            // in the object itself.
+            deleteItem(req.fileData.content, req.allParams.slice(1));
+            
+            // Defining the deleteItem function, as described above
+            function deleteItem(contentFileData, encapsulationData){
+                // Determining the itemName and parentItem name, based on the last
+                // two values of the encapsulationData data (parameters of the request URL).
+                // Parent name may not always have a value, as the item may be at the top
+                // level of the content object, so defaulting this to null if no value exits
+                var itemName = encapsulationData[encapsulationData.length-1];
+                var parentName = encapsulationData[encapsulationData.length-2] || null;
 
-// Request to delete an item from a collection within a project
-router.delete("/:projectID/:collection/:item", function(req, res, next){
-    // Temporarily storing the collection name (as requested in the URL parameters) on
-    // the request object, so that it can be used throughout this route. Transforming
-    // the collection name to lowercase, for use within the route. 
-    req.collectionName = req.params.collection.toLowerCase();
+                // Creating an removedItemFrom property on the request object, to track if and
+                // when an item is deleted within this function (so as to determine if the 
+                // content file needs to be updated)
+                req.removedItemFrom = null;
 
-    // Temporarily storing the item name (as requested in the URL parameters) on
-    // the request object, so that it can be used throughout this route. Transforming
-    // the item name to lowercase, for use within the route. 
-    req.itemName = req.params.item.toLowerCase();
+                // Looping through the encapsulationData array (i.e. parameters passed to the
+                // request URL) to drill down into the fileData object, to find the property that
+                // needs to have an item removed from it. 
+                for(var i=0; i<encapsulationData.length; i++){
+                    // Not including the last two indexes of the
+                    // array, as these will be the parentName and itemName (as referenced above). By
+                    // the end of this loop, I should have the item from which the item should
+                    // be removed.
+                    if(i < encapsulationData.length - 2){
+                        if(contentFileData[encapsulationData[i]] != null){
+                            // Setting the contentFileData equal to the next level of the encapsulationData array
+                            // i.e. to keep drilling down into the contentFileData object
+                            contentFileData = contentFileData[encapsulationData[i]];
+                        } else {
+                            req.feedsErrors.push("'" + encapsulationData.slice(0, encapsulationData.length-1)[0].join("/") + "' does not exist. Please create the container objects first");
+                            return;
+                        }
+                        
+                    }
+                }
 
-    // Checking if the user has the appropriate access level to be able to delete
-    // an item from this project
-    switch(req.user_access_level) {
-        default: {
-            // Creating a temporary variable, to store the current project content, so
-            // that it can be updated based on the request
-            var updatedProjectContent = req.fileData.content;
-                    
-            // Determining what data type the collection that this item exists within is,
-            // so as to decide how best to remove this item from it
-            switch(updatedProjectContent[req.collectionName].constructor){
-                case Array: {
-                    console.log("array");
-                    // Accessing the requested collection, from the temporary project content
-                    // file, and splicing the requested item from it (removing only 1)
-                    updatedProjectContent[req.collectionName].splice([req.itemName], 1);
+                // Checking whether the fileData object contains the property referred to in the parent
+                // name variable i.e. does the request new item have a parent, or is it at the top-most
+                // level of the project_structure object
+                if(contentFileData[parentName] != null){
+                    // Determing what type of item we are trying to remove the item from i.e.
+                    // an Array, Object or other.
+                    switch(contentFileData[parentName].constructor.name.toLowerCase()){
+                        case "array": {
+                            // Checking if the array we are trying to remove this item from contains an index
+                            // with the same value as the itemName
+                            if(contentFileData[parentName][itemName] != undefined){
+                                contentFileData[parentName].splice(itemName, 1);
+                            } else {
+                                // Since this index does not exist within this array, adding this to the feedsErrors
+                                // array, and then returning this function so that no further attempt to create this
+                                // item will be made. Since req.newItem will remain null, this error will be returned
+                                // to the caller, further down along this route
+                                req.feedsErrors.push(itemName + " does not exist within " + parentName + " and so cannot be removed");
+                                return;
+                            } 
+                            break;                   
+                        }
+                        case "object": {
+                            // Checking if the object we are trying to remove this item from contains a property
+                            // with the same value as the itemName
+                            if(contentFileData[parentName][itemName] != undefined){
+                                delete contentFileData[parentName][itemName];
+                            } else {
+                                // Since this property does not exist within this object, adding this to the feedsErrors
+                                // array, and then returning this function so that no further attempt to create this
+                                // item will be made. Since req.newItem will remain null, this error will be returned
+                                // to the caller, further down along this route
+                                req.feedsErrors.push(itemName + " does not exist within " + parentName + " and so cannot be removed");
+                                return;
+                            }
+                            break;
+                        }
+                        default: {
+                            res.send("uh oh");
+                        }
+                    }
+
+                    // If the function has reached this point, then an item must have been deleted (as
+                    // if this proved not to be possible, then the function would have returned by now).
+                    // Setting this removedItemFrom property of the request object to be an empty object. Then
+                    // creating a new property on this object, using the name of the parent (as this 
+                    // will be the item from which the item was removed). Setting the value of this
+                    // new property to be equal to the parent item we just removed the item from i.e. so
+                    // that it now contains all of it's existing properties/indexes, minus the item
+                    // we removed. Doing this in two seperate steps, as the parentName is being generated
+                    // dynamically, so req.newItem.parentName would not be sufficient.
+                    req.removedItemFrom = {};
+                    req.removedItemFrom[parentName] = contentFileData[parentName];
+                } else {
+                    // Since this item does not appear to have a parent, checking whether it exists
+                    // at the top level of the file data project_structure i.e. is there a property by this 
+                    // name already defined
+                    if(contentFileData[itemName] != undefined){
+                        delete contentFileData[itemName];
+                    } else {
+                        // Since this property does not exist on the global object, adding this to the feedsErrors
+                        // array, and then returning this function so that no further attempt to create this
+                        // item will be made. Since req.newItem will remain null, this error will be returned
+                        // to the caller, further down along this route
+                        req.feedsErrors.push(itemName + " does not exist and so cannot be removed");
+                        return;
+                    }
+
+                    // If the function has reached this point, then an item must have been deleted (as
+                    // if this proved not to be possible, then the function would have returned by now).
+                    // Setting this removedItemFrom property of the request object to be equal to the content
+                    // file data that we just removed the item from.
+                    req.removedItemFrom = contentFileData;
                 }
             }
-
             
-            // Checking that a change has been made to the project content i.e. that it
-            // is not the same as it was when the file was first read in at the beginning of
-            // this request
-            if(JSON.stringify(updatedProjectContent) != req.fileData.content){
-                // Updating the content.json file of the project, with the updated project content
-                // as a JSON string
-                fs.writeFile("./projects/" + req.params.projectID + "/content.json", JSON.stringify(updatedProjectContent), function(err){
-                    if(err) {
-                        // Logging the error to the console
-                        console.log("Error updating content.json file " + err);
+            // Checking if the removedItemFrom property on the request object is still null
+            // i.e. has an item been removed? If not, then there will be no need to
+            // update the project content file.
+            if(req.removedItemFrom != null){
+                // Checking that the fileData content is still a valid object, by attempting
+                // to parse it to JSON. Since the new item that was created will have been reflected
+                // in this object (as it was passed by reference to a function) completing this recheck
+                // to ensure that creating the new item did not corrupt the existing content
+                if(validation.objectToJson(req.fileData.content)){
+                    // Updating this project's content.json file, passing the JSON stringified version
+                    // of the fileData content object as the contents
+                    fs.writeFile("./projects/" + req.params.projectID + "/content.json", JSON.stringify(req.fileData.content), function(err){
+                        if(err) {
+                            // Logging the error to the console
+                            console.log("Error updating project content file " + err);
 
-                        // Unable to update the project content.json file. Adding this as 
-                        // an error to the feedsErrors array.
-                        req.feedsErrors.push("Server error - unable to update the project content");
+                            // As it has not been possible to update the content.json file for this 
+                            // project, adding this as an error to the feedsErrors array.
+                            req.feedsErrors.push("Server error - unable to create new item in collection");
 
-                        // Since this is a significant issue, passing this request to the feeds-errors
-                        // route, by calling the next method with an empty error (as all errors will be
-                        // accessible from the feedsErrors array).
-                        next(new Error());
-                    } else {
-                        console.log("Project content file updated");
+                            // Since this is a significant issue, passing this request to the feeds-errors
+                            // route, by calling the next method with an empty error (as all errors will be
+                            // accessible from the feedsErrors array).
+                            return next(new Error());
+                        } else {
+                            console.log("New item added to project");
 
-                        // Returning the updated project collection, that no longer contains this item,
-                        // as the response to the request
-                        res.send(updatedProjectContent[req.collectionName]);
-                    }
-                });
+                            res.send(req.removedItemFrom);
+                        }
+                    });
+                } else {
+                    // As it has not been possible parse the updated object to JSON, the content
+                    // cannot be updated. Adding this as an error to the feeds errors array.
+                    req.feedsErrors.push("The content included is not valid JSON. Cannot add to collection");
+
+                    // Since this is a significant issue, passing this request to the feeds-errors
+                    // route, by calling the next method with an empty error (as all errors will be
+                    // accessible from the feedsErrors array).
+                    return next(new Error());
+                }
+            } else {
+                // Returning any errors set in the createItem() function to the caller
+                return next(new Error());
             }
+                
 
-            break;
-        }
-    }          
+
+        } else {
+            // Adding this as an error to the feeds errors array.
+            req.feedsErrors.push("This user does not have permission to add to this collection");
+
+            // Since this is a significant issue, passing this request to the feeds-errors
+            // route, by calling the next method with an empty error (as all errors will be
+            // accessible from the feedsErrors array).
+            return next(new Error());
+        }   
+
+    } else {
+        // Adding this as an error to the feeds errors array.
+        req.feedsErrors.push("Not enough parameters provided to create this content");
+
+        // Since this is a significant issue, passing this request to the feeds-errors
+        // route, by calling the next method with an empty error (as all errors will be
+        // accessible from the feedsErrors array).
+        next(new Error());
+    }
 });
 
 // Exporting the router that was set up in this file, so that it can be included
