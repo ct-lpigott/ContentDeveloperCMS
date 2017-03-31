f8fc8a6f9318b518feb0bfb0312a380ff1f2da62
Laura Pigott <pigottlaura@gmail.com>
2017-03-31 14:28:37 +0100

    Logging errors only - if no response object

--- routes/components/error-routes/feeds-errors.js
+++ routes/components/error-routes/feeds-errors.js
@@ -9,49 +9,53 @@ module.exports = function (err, req, res, next) {
         res.status(err.status || 500);
     }
 
-    // Creating the errors array on the response object
-    req.responseObject.errors = [];
+    if(req.responseObject != null){
+        // Creating the errors array on the response object
+        req.responseObject.errors = [];
 
-    if(req.loginErrors != null){
-        // Adding any errors that occurred within the login route, so that they
-        // can be passed back to the user.
-        for(var i=0; i<req.loginErrors.length; i++){
-            req.responseObject.errors.push(req.loginErrors[i]);
+        if(req.loginErrors != null){
+            // Adding any errors that occurred within the login route, so that they
+            // can be passed back to the user.
+            for(var i=0; i<req.loginErrors.length; i++){
+                req.responseObject.errors.push(req.loginErrors[i]);
+            }
+            
         }
         
-    }
-    
-    if(req.feedsErrors != null){
-        // Adding any errors that occurred within the feeds route, so that they
-        // can be passed back to the user.
-        for(var i=0; i<req.feedsErrors.length; i++){
-            req.responseObject.errors.push(req.feedsErrors[i]);
+        if(req.feedsErrors != null){
+            // Adding any errors that occurred within the feeds route, so that they
+            // can be passed back to the user.
+            for(var i=0; i<req.feedsErrors.length; i++){
+                req.responseObject.errors.push(req.feedsErrors[i]);
+            }
+            
         }
-        
-    }
-    if(req.adminErrors != null){
-        // Adding any errors that occurred within the admin route, so that they
-        // can be passed back to the user.
-        for(var i=0; i<req.adminErrors.length; i++){
-            req.responseObject.errors.push(req.adminErrors[i]);
+        if(req.adminErrors != null){
+            // Adding any errors that occurred within the admin route, so that they
+            // can be passed back to the user.
+            for(var i=0; i<req.adminErrors.length; i++){
+                req.responseObject.errors.push(req.adminErrors[i]);
+            }
         }
-    }
-    if(req.preRequestErrors != null){
-        // Adding any errors that occurred within the pre request routes, so that they
-        // can be passed back to the user.
-        for(var i=0; i<req.preRequestErrors.length; i++){
-            req.responseObject.errors.push(req.preRequestErrors[i]);
-        } 
-    }   
+        if(req.preRequestErrors != null){
+            // Adding any errors that occurred within the pre request routes, so that they
+            // can be passed back to the user.
+            for(var i=0; i<req.preRequestErrors.length; i++){
+                req.responseObject.errors.push(req.preRequestErrors[i]);
+            } 
+        }   
 
-    // If a login is required, adding a loginRequired property to the response
-    // to notify the user and/or CMS
-    if(err.message == "loginRequired"){
-        req.responseObject.loginRequired = true;
-    }
+        // If a login is required, adding a loginRequired property to the response
+        // to notify the user and/or CMS
+        if(err.message == "loginRequired"){
+            req.responseObject.loginRequired = true;
+        }
 
-    // Sending the responseObject back as the response for this request. This 
-    // will contain the array of any errors that have occurred within the feeds
-    // route
-    res.send(req.responseObject);
+        // Sending the responseObject back as the response for this request. This 
+        // will contain the array of any errors that have occurred within the feeds
+        // route
+        res.send(req.responseObject);
+    } else {
+        console.log(err);
+    }
 };
