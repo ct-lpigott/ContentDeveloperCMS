277b3f462c104f38e87918e873bd957a12882a28
Laura Pigott <pigottlaura@gmail.com>
2017-04-03 20:29:18 +0100

    Rebuild of Angular app
    Notifying app component of impending timout and eventual timeout

--- public/cms/main.bundle.js
+++ public/cms/main.bundle.js
@@ -93,6 +93,7 @@ var ContentDeveloperServerService = (function () {
         this._contentErrors = {};
         this._serverSessionMaxSeconds = 15; //60 * 30
         this._warnTimeoutAt = 0.80; // Percentage of server session max time
+        this._warnTimeoutSent = false;
         this._headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Headers */]();
         this._headers.append("Content-Type", "application/json");
     }
@@ -110,6 +111,9 @@ var ContentDeveloperServerService = (function () {
     };
     ContentDeveloperServerService.prototype.setupLogoutCallback = function (appComponentLogoutFunction) {
         this._notifyAppComponentOfLogout = appComponentLogoutFunction;
+    };
+    ContentDeveloperServerService.prototype.setTimoutWarningCallback = function (appComponentTimoutWarningFunction) {
+        this._notifyAppComponentOfImpendingTimeout = appComponentTimoutWarningFunction;
     };
     ContentDeveloperServerService.prototype.getLoginUrl = function () {
         var requestUrl = this._serverUrl + "/admin/loginUrl";
@@ -135,13 +139,17 @@ var ContentDeveloperServerService = (function () {
                 _this._resetIntervalTimer();
                 _this._activeSessionInterval = setInterval(function () {
                     _this._activeSessionTime++;
-                    if (_this._activeSessionInterval > _this._serverSessionMaxSeconds) {
-                        clearInterval(_this._activeSessionInterval);
+                    if (_this._activeSessionTime > _this._serverSessionMaxSeconds) {
+                        _this._stopIntervalTimer();
+                        _this._notifyAppComponentOfImpendingTimeout("Your session has expired", true);
                         console.log("Your session has expired");
                     }
-                    else if (_this._activeSessionTime > (_this._serverSessionMaxSeconds * _this._warnTimeoutAt)) {
-                        var remainingMinutes = _this._serverSessionMaxSeconds - _this._activeSessionTime;
-                        console.log("Your session will expire in " + remainingMinutes + " minutes");
+                    else if (!_this._warnTimeoutSent) {
+                        if (_this._activeSessionTime > (_this._serverSessionMaxSeconds * _this._warnTimeoutAt)) {
+                            var remainingMinutes = (_this._serverSessionMaxSeconds - _this._activeSessionTime) / 60;
+                            _this._notifyAppComponentOfImpendingTimeout("Your session will expire in " + remainingMinutes + " minutes", false);
+                            _this._warnTimeoutSent = true;
+                        }
                     }
                 }, 1000);
                 _this._currentUser = responseObject.user;
@@ -152,8 +160,14 @@ var ContentDeveloperServerService = (function () {
     ContentDeveloperServerService.prototype._resetIntervalTimer = function () {
         this._activeSessionTime = 0;
     };
+    ContentDeveloperServerService.prototype._stopIntervalTimer = function () {
+        this._activeSessionTime = 0;
+        clearInterval(this._activeSessionInterval);
+        this._warnTimeoutSent = false;
+    };
     ContentDeveloperServerService.prototype.logout = function () {
         var _this = this;
+        this._stopIntervalTimer();
         var logoutUrl = this._serverUrl + "/admin/logout";
         var logoutObservable = this._http
             .get(logoutUrl)
@@ -1012,6 +1026,9 @@ var AppComponent = (function () {
         var _this = this;
         this._cdService.setupLogoutCallback(function () {
             _this.loginRequired();
+        });
+        this._cdService.setTimoutWarningCallback(function (warning, sessionExpired) {
+            console.log(warning);
         });
         this._cdService.loadUser().subscribe(function (responseObject) {
             if (responseObject.loginRequired) {
