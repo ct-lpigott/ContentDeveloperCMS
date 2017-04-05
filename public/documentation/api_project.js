df844b2be685ce049c7b203adbd021bb714b3e46
Laura Pigott <pigottlaura@gmail.com>
2017-04-05 15:44:20 +0100

    Updating documentation
    Favicon

--- public/documentation/api_project.js
+++ public/documentation/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-04-05T13:22:18.302Z",
+    "time": "2017-04-05T14:36:58.572Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
